76401f73d2debb581ef9e2876a0abca38438a138
Kenneth Jackson <kjackson@connectionsis.com>
2018-12-08 02:14:46 -0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kenneth W Jackson on 12/8/18.
+//  Copyright © 2018 Connections Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
