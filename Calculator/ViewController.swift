523e5e39d643fe0fe7e4b7dc45194ce731da72ae
Kenneth Jackson <kjackson@connectionsis.com>
2019-01-27 22:52:09 -0600

    Calculations Code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var numberOnScreen: Double = 0
+    var previousNumber: Double = 0
+    var operation = 0
+    var isPerformingOperation = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultsLabel.text = ""
+        
     }
 
+    @IBAction func numberPressed(_ sender: Any) {
+        // Write number string to screen
+        
+        let tag = (sender as! UIButton).tag
+        
+        if isPerformingOperation == true {
+            isPerformingOperation = false
+            resultsLabel.text = String(tag-1)
+            numberOnScreen = Double(resultsLabel.text!)!
+        } else {
+            resultsLabel.text = resultsLabel.text! + String(tag - 1)
+            numberOnScreen = Double(resultsLabel.text!)!
+        }
 
+        
+    }
+    
+    @IBAction func operatorPressed(_ sender: Any) {
+        
+        // Implement operators
+        let tag = (sender as! UIButton).tag
+        
+        // Clear button
+        if tag == 16 {
+            resultsLabel.text = ""
+            previousNumber = 0
+            numberOnScreen = 0
+            operation = 0
+        }
+        
+        // Addition operator
+        if tag == 11 {
+            isPerformingOperation = true
+            previousNumber = Double(resultsLabel.text!)!
+            resultsLabel.text = "÷"
+            operation = tag
+        } else if tag == 12 {
+            isPerformingOperation = true
+            previousNumber = Double(resultsLabel.text!)!
+            resultsLabel.text = "X"
+            operation = tag
+        } else if tag == 13 {
+            isPerformingOperation = true
+            previousNumber = Double(resultsLabel.text!)!
+            resultsLabel.text = "-"
+            operation = tag
+        } else if tag == 14 {
+            isPerformingOperation = true
+            previousNumber = Double(resultsLabel.text!)!
+            resultsLabel.text = "+"
+            operation = tag
+        } else if tag == 15 {
+           
+            if operation == 11 {
+                resultsLabel.text = String(previousNumber / numberOnScreen)
+                
+            } else if operation == 12 {
+                resultsLabel.text = String(previousNumber * numberOnScreen)
+                
+            } else if operation == 13 {
+                resultsLabel.text = String(previousNumber - numberOnScreen)
+                
+            } else if operation == 14 {
+                resultsLabel.text = String(previousNumber + numberOnScreen)
+            }
+        }
+    }
 }
 
